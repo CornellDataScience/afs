7de0e12362ad200f194824f8191310017d13a18a
yingyi66 <yingyi0528@gmail.com>
2020-03-08 12:23:34 -0400

    added send message to vice

--- venus.py
+++ venus.py
@@ -1,11 +1,10 @@
-import socket 
-import select 
+import socket
 
 HEADER_LENGTH = 10
 IP = "127.0.0.1"
 PORT = 1234
 
-# Initialize and bind socket for stream and resuse same socket for 
+# Initialize and bind socket for stream and resuse same socket for
 # reconnection if it goes down
 venus_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 venus_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
@@ -13,7 +12,7 @@ venus_socket.bind((IP,PORT))
 
 #All sockets including venus
 sockets_list = [venus_socket]
-#client socket is key and data is value 
+#client socket is key and data is value
 vice_data = {}
 
 def receive_message(vice_socket):
@@ -22,16 +21,16 @@ def receive_message(vice_socket):
     #If no data is given
     if not len(message_header):
       return False
-    else: 
+    else:
         message_length = int(message_header.decode("utf-8"))
         #Problematic because this assumes small messages, not the case for files
         # Have to figure out buffering system 
         return {"header":message_header, "data":vice_socket.recv(message_length)}
   except:
-    return False 
+    return False
 
-while True: 
-  #select.select takes first a read list and a write list 
+while True:
+  #select.select takes first a read list and a write list
   read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
   for incoming_socket in read_sockets:
     if incoming_socket == venus_socket:
@@ -39,8 +38,8 @@ while True:
       client_data = receive_message(vice_socket)
 
       if client_data is False:
-        continue 
+        continue
 
-      sockets_list.append(vice_socket)
+      sockets_list.append(vice_s32ocket)
       vice_data[vice_socket] = client_data
       print("Message received")
--- vice.py
+++ vice.py
@@ -1,14 +1,25 @@
-import socket 
+import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(),1234))
+vice_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+vice_socket.connect((socket.gethostname(),1234))
+#think about the directories are array of arrays
+home_directory = [[],[],[]]
+#the first index in the array is the hashed name of the directory
+home_directory[0][0] = hash('desired_directory')
 
-while True: 
-  msg = s.recv(1024)
-  print(msg.decode("utf-8"))
-
-def open(file_name): 
+def open(file_name):
   s.send(file_name)
   while True:
     msg = s.recv(1024)
     print(msg.decode("utf-8"))
+
+
+
+def send_message(directoryFID, desired_directory):
+    if directoryFID.equals(homeFID):
+        desired_d = hash(desired_directory)
+        for i in home_directory:
+            if home_directory[i][0] == desired_d:
+                #set up callbacks on the directory and the client
+                #how to generate FID or what is the FID of each directories and files
+                return home_directory[i][1:]
