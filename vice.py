4113c15694c5944627463afe3b8654cf6e2da70a
Anoop Balakrishnan <ab2226@cornell.edu>
2020-04-20 18:11:13 -0700

    switch venus and vice

--- vice.py
+++ vice.py
@@ -1,13 +1,16 @@
 import socket
 import select
-import time
-import json 
+import json
+import enum
 
-#creating a hashtable of files
-dict = {}
-dict['a'] = 'alpha.txt'
-dict['g'] = 'gamma.txt'
-dict['o'] = 'omega.txt'
+HEADER_LENGTH = 10
+TYPE_LENGTH = 1
+IP = "127.0.0.1"
+PORT = 1234
+class message_type(enum.Enum):
+  file_request = 1
+  file_update = 2
+
 
 
 with open('parameter.json') as f:
@@ -15,28 +18,53 @@ with open('parameter.json') as f:
 HEADER_LENGTH = data['HEADER_LENGTH']
 IP = data["IP"]
 PORT = data['PORT']
-CONNECTION = data['CONNECTION']
 
+# Initialize and bind socket for stream and resuse same socket for
+# reconnection if it goes down
+venus_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+venus_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+venus_socket.bind((IP,PORT))
 
+#All sockets including venus
+sockets_list = [venus_socket]
+#client socket is key and data is value
+vice_data = {}
 
-HEADER_LENGTH = 10
-PORT = 1234
-#our server will receive up to 5 connections
-CONNECTION = 5
-# fetch request of the file
-reuqested_file = 'a'
-
-#Initialize and connect socket
-vice_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-vice_socket.bind((socket.gethostname(),PORT))
-vice_socket.listen(CONNECTION)
+def receive_message(vice_socket):
+  try:
+    message_header = vice_socket.recv(HEADER_LENGTH)
+    #If no data is given
+    if not len(message_header):
+      return False
+    else:
+        message_length = int(message_header.decode("utf-8"))
+        #Problematic because this assumes small messages, not the case for files
+        # Have to figure out buffering system 
+        return {"header":message_header, "data":vice_socket.recv(message_length)}
+  except:
+    return False
 
 while True:
-    clientsocket, address = vice_socket.accept()
-    print(f"Connection from {address} has been established")
+  #select.select takes first a read list and a write list
+  read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+  for incoming_socket in read_sockets:
+    if incoming_socket == venus_socket:
+      vice_socket, vice_address = venus_socket.accept()
+      client_data = receive_message(vice_socket)
 
-    msg = dict[reuqested_file]
-    msg = f'{len(msg):<{HEADER_LENGTH}}' + time.time() + msg
-    clientsocket.send(bytes(msg, "tuf-8"))
+      if client_data is False:
+        continue
 
-#question: how to differentiate which files to send to different clients
+      sockets_list.append(vice_socket)
+      vice_data[vice_socket] = client_data
+      print("Message received")
+    else: 
+      message = receive_message(incoming_socket)
+      if message is False:
+        print(f"Closed Connection from {vice_data[incoming_socket]['data'].decode('utf-8')}")
+        sockets_list.remove(incoming_socket)
+        del vice_data[incoming_socket]
+        continue
+      user = vice_data[incoming_socket]
+      print(f"Received message from {user['data'].decode('utf-8')}")
+      #Below here is where you place message handling/protocols/propogation 
