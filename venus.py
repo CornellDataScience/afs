761516b7199c482fae8e7c860cd7cf0d5bd1fb8a
Abhinav Prasad <ap742@cornell.edu>
2020-04-27 20:31:50 -0400

    venusfile open

--- venus.py
+++ venus.py
@@ -16,7 +16,7 @@ PORT = 1234
 # all the files stored locally
 venus_data = {}
 
-vice_file = FUSE(filesystem.Filesystem(root), "/", nothreads=True,
+venus_file = FUSE(venusfs.VenusFilesystem(root), "/", nothreads=True,
          foreground=True, **{'allow_other': True})
 
 class myMessage:
@@ -27,14 +27,14 @@ class myMessage:
         #whether the file has been changed(0 means no change)
         self.change = change
     #create a json message to send
-    def msg():
+    def msg(self):
         msg = {}
         msg['length'] = len(file_name)
         msg['type'] = self.type
         msg['time'] = time.time()
         msg['data'] = str(self.file_name)
         return json.dump(msg)
-    def covertStr():
+    def covertStr(self):
         return str(self.messages)
 
 #Initialize and connect socket
@@ -57,15 +57,13 @@ def receive_message(vice_socket):
   except:
     return False
 
+#can move this into venusfs and just use venus_file.open(file_name.convertStr())
 def open(file_name):
     if file_name.convertStr() in venus_data.key():
         #check whether the file is stored locally
         file = venus_data.get[file_name]
         print ("file in local cache")
         return False
-        #venusfs
-
-
     else:
         # if not, make it a request message and run the request func
         file_name.type = "request"
@@ -75,22 +73,22 @@ def request(request_msg):
     venus_socket, address = vice_socket.accept()
     #create dic for the message
     message = request_msg.msg()
-    serversocket.send(bytes(message, "utf-8")
-    print ("file not in local cache, request sent")
+    serversocket.send(bytes(message, "utf-8")) #should this be vice_socket
+    print("file not in local cache, request sent")
 
 
 while True:
-    input_type = input("Please enter the type of action you would like to do ('open', 'close')":)
-    input_message = input("Please enter the file name":)
+    input_type = input("Please enter the type of action you would like to do ('open', 'close'):")
+    input_message = input("Please enter the file name:")
     # initialize the input message as myMessage class
     message = myMessage(input_message, input_type)
-    if message.type = "open":
+    if message.type == "open":
         open(message)
-    if message.type = "request":
+    if message.type == "request":
         request(message)
         server_data = receive_message(venus_socket)
         if server_data is False:
             continue
         print(f"Received message from the server")
         server_message = myMessage(server_data['body'], server_data['type'])
-        venus_data[server_message.convertStr()] = #not sure how to express the content of the file
+        venus_data[server_message.convertStr()] = None #not sure how to express the content of the file
--- venusfs.py
+++ venusfs.py
@@ -1,6 +1,20 @@
 from fuse import FUSE, FuseOSError, Operations
 from filesystem import Filesystem
 import venus
+import os
 
 class VenusFilesystem(Filesystem):
+    def __init__(self, root, localfiles):
+        self.root = root
+        self.local = localfiles
 
+    def open(self, path, flags=None):
+        if path in self.local.key():
+            #check whether the file is stored locally
+            __file = os.open(self._full_path(self.get[path]),flags)
+            print ("file in local cache")
+            return __file
+        else:
+            # if not, make it a request message and run the request func
+            file_name.type = "request"
+            venus.request() #how do requests work
